f0cb1dee74acbed719c0c3784c97622f857b4f9b
blamassa <brenoelamassa@gmail.com>
2018-02-18 09:07:50 -0300

    edited /home/index and removed $ from url

--- finances/urls.py
+++ finances/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('home.urls')),
+    url(r'^', include('home.urls')),
     url(r'^budgetlite/', include('budgetlite.urls')),
 ]
